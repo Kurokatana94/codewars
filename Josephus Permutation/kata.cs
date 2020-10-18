3f67b5349f6c56afb885e4231a2c27ebb3ec1b03
Timoty Ravoni <70312018+Kurokatana94@users.noreply.github.com>
2020-10-18 20:14:09 +0100

    Update kata.cs

--- Josephus Permutation/kata.cs
+++ Josephus Permutation/kata.cs
@@ -4,14 +4,12 @@ public class Josephus
 {
    public static List<object> JosephusPermutation(List<object> items, int k)
    {
-     Console.WriteLine(" "+k.ToString() + " " + string.Join(",", items));
      var result = new List<object>();
      int n = 0;
      int iterations=items.Count;     
      for(int i=0;i<iterations;i++){
        n+=k-1;
        while(n>=items.Count) n -= items.Count;
-       Console.WriteLine(items[n]);
        result.Add(items[n]);
        items.RemoveAt(n);
        if(n==items.Count) n-=items.Count;
